2fd6bf16bc9f1d03ed4a2d31c27822ea17cf6d06
Sathwik Reddy <121279419+sathwik789@users.noreply.github.com>
2025-03-03 20:04:25 +0530

    BINARY SUBARRAYS WITH SUM

---
+++ PROBLEM 930.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int helper(vector<int>& nums, int goal){
+        if(goal < 0) return 0;
+        int l =0;
+        int r =0;
+        int cnt =0;
+        int sum =0;
+        while(r < nums.size()){
+            sum += nums[r];
+            while(sum > goal  && l <= r) {
+                sum = sum - nums[l];
+                l = l+1;
+            }
+            cnt += (r -l +1);
+            r++;
+        }
+        return cnt;
+    }
+    int numSubarraysWithSum(vector<int>& nums, int goal) {
+        return helper(nums,goal) - helper(nums,goal -1);
+    }
+};
